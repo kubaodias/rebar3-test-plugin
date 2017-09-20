b6d2216fb6fa6976403bdb72919667b61dc0fddb
Kuba Odias <kuba.odias@relayr.io>
2017-09-20 14:40:05 +0200

    set proj apps

--- src/test_prv.erl
+++ src/test_prv.erl
@@ -33,10 +33,9 @@ do(State) ->
     % rebar_api:info("~p DEPS (~p): ~p~n", [length(Deps), DepNames, Deps]),
     lists:foldl(fun(AppInfo, {ok, StateAcc}) ->
             rebar_api:info("Switch to ~p (~p)~n", [rebar_app_info:name(AppInfo), rebar_app_info:dir(AppInfo)]),
-            State1 = rebar_state:current_app(StateAcc, AppInfo),
-            State2 = rebar_state:dir(State1, rebar_app_info:dir(AppInfo)),
-            rebar_api:info("PR APPS ~p~n", [rebar_state:project_apps(State2)]),
-            rebar_prv_eunit:do(State2)
+            State1 = rebar_state:project_apps(StateAcc, [AppInfo]),
+%            State2 = rebar_state:dir(State1, rebar_app_info:dir(AppInfo)),
+            rebar_prv_eunit:do(State1)
         end, {ok, State}, Apps ++ Deps).
 
 -spec format_error(any()) ->  iolist().
