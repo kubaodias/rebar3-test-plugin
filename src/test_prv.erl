0f7dfc50da5a6ec775e810bc3b0c176bd87d27b5
Kuba Odias <kuba.odias@relayr.io>
2017-09-20 05:13:48 +0200

    install deps

--- src/test_prv.erl
+++ src/test_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, test).
--define(DEPS, [app_discovery]).
+-define(DEPS, [install_deps]).
 
 %% ===================================================================
 %% Public API
